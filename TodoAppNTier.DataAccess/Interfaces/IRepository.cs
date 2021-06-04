d7e4096239cc62b922d3c8bc2f06f46e4cfd864f
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2021-06-04 13:38:26 +0300

    Repositories

---
+++ TodoAppNTier.DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoAppNTier.DataAccess.Interfaces
+{
+    public interface IRepository<T> where T : class, new()
+    {
+        Task<List<T>> GetAll();
+        Task<T> GetById(object id);
+        Task<T> GetByFilter(Expression<Func<T, bool>> filter, bool asNoTracking = false);
+        Task Create(T entity);
+        Task Update(T entity);
+        Task Remove(T entity);
+    }
+}
